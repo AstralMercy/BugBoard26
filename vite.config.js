90e9dfeccf4b2bf324d65ed8d52b85c2a241cd27
gaeconci <gaeconci@gmail.com>
2026-06-17 21:09:00 +0200

    Implement Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,2 +1,8 @@
-// Module scaffold
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+})
