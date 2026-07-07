5fe37309fa0af81f5075bb1fc0ff5bdc13e2d1ba
gaeconci <gaeconci@gmail.com>
2026-07-07 19:00:00 +0200

    Implement Jest config

--- server/jest.config.cjs
+++ server/jest.config.cjs
@@ -1,2 +1,31 @@
-module.exports = {};
-
+module.exports = {
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests'],
+  testMatch: ['**/*.test.js'],
+  transform: {},
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        outputDirectory: '<rootDir>/test-results',
+        outputName: 'junit.xml',
+        suiteNameTemplate: '{filename}',
+        classNameTemplate: '{classname}',
+        titleTemplate: '{title}',
+        addFileAttribute: 'true',
+      },
+    ],
+  ],
+  collectCoverageFrom: ['utils/**/*.js'],
+  coverageDirectory: '<rootDir>/coverage',
+  coverageReporters: ['text', 'html', 'lcov', 'cobertura'],
+  coverageThreshold: {
+    global: {
+      statements: 90,
+      branches: 85,
+      functions: 100,
+      lines: 90,
+    },
+  },
+};
